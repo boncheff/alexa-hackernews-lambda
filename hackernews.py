ef562e917a21b385983f5a4f720052a408d2c0cd
boncheff <boncheff@users.noreply.github.com>
2019-05-29 20:30:37 +0100

    Update hackernews.py

--- hackernews.py
+++ hackernews.py
@@ -109,7 +109,7 @@ def get_welcome_response():
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to Hacker News"
+    speech_output = "Welcome to HackerNews"
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
     reprompt_text = speech_output
